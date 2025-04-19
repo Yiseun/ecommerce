38aed9e4c090f4dbf47a70eaa8089cd6ea8e92c1
Seun Yi <yiseun10@gmail.com>
2025-04-19 11:40:43 +0900

    feat : 디펜던시 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("java")
+    id("org.springframework.boot") version "3.2.0"
+    id("io.spring.dependency-management") version "1.1.2"
 }
 
 group = "com.ecommerce"
@@ -10,6 +12,9 @@ repositories {
 }
 
 dependencies {
+    compileOnly("org.projectlombok:lombok:1.18.30")
+    implementation("org.springframework.boot:spring-boot-starter-web:3.2.0")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
