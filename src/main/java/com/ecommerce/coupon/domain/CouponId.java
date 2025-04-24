2561a568a3703773c7eaec2c9404b2fd560ca40b
Seun Yi <yiseun10@gmail.com>
2025-04-24 21:34:10 +0900

    feat : coupon 도메인 생성

---
+++ src/main/java/com/ecommerce/coupon/domain/CouponId.java
@@ -0,0 +1,36 @@
+package com.ecommerce.coupon.domain;
+
+import com.ecommerce.coupon.exception.domain.FailedCreationException;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+@EqualsAndHashCode
+@Getter
+public class CouponId {
+    private static final Long MINIMUM_ID_VALUE = 0L;
+    private final Long value;
+
+    private CouponId(final String value){
+        this.value = validateValue(value);
+    }
+
+    private Long validateValue(final String value){
+        try {
+            final Long longValue = Long.valueOf(value);
+            if(longValue<MINIMUM_ID_VALUE){
+                throw new FailedCreationException("Id가 유효하지 않습니다. id값은"+value);
+            }
+            return longValue;
+        }catch (NumberFormatException e){
+            throw new FailedCreationException("Id가 유효하지 않습니다. id값은"+value);
+        }
+    }
+
+    public static CouponId from(final String value){
+        return new CouponId(value);
+    }
+
+    public static CouponId createEmptyId(){
+        return new CouponId(null);
+    }
+}
