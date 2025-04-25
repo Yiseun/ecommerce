5f6e72a036ad48933b47451ef69113c385edf95e
Seun Yi <yiseun10@gmail.com>
2025-04-25 23:01:15 +0900

    feat : payment paymentItem domain 추가

---
+++ src/main/java/com/ecommerce/payment/domain/paymentitem/Price.java
@@ -0,0 +1,27 @@
+package com.ecommerce.payment.domain.paymentitem;
+
+import com.ecommerce.payment.exception.domain.FailedCreationException;
+import lombok.Getter;
+
+@Getter
+public class Price {
+    private static final Long MINIMUM_PURCHASE_PRICE = 0L;
+    private final Long value;
+    private Price(final String value){
+        this.value = validate(value);
+    }
+    private Long validate(final String value){
+        try {
+            final long longValue = Long.parseLong(value);
+            if(longValue<MINIMUM_PURCHASE_PRICE){
+                throw new FailedCreationException("구매가격이 0원보다 작을수는 없습니다.");
+            }
+            return longValue;
+        }catch (NumberFormatException e){
+            throw new FailedCreationException("가격 형식이 잘못됐습니다.");
+        }
+    }
+    public static Price from(final String value){
+        return new Price(value);
+    }
+}
