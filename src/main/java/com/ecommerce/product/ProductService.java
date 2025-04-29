7c97a8372f6f15be66b3c1b8d1956e7ae8111791
이세운 <Yiseun10@gmail.com>
2025-04-29 19:21:35 +0900

    Feat/ Product update 기능 추가 (#5)
    * feat : product dto 추가
    
    * feat : product receiver에update 메서드 추가
    
    * feat : product service에 update 메서드 추가
    
    * feat : product update 메서드, 생성자 추가

--- src/main/java/com/ecommerce/product/ProductService.java
+++ src/main/java/com/ecommerce/product/ProductService.java
@@ -2,7 +2,9 @@ package com.ecommerce.product;
 
 import com.ecommerce.grobal.util.NonDuplicatedList;
 import com.ecommerce.product.domain.Product;
+import com.ecommerce.product.dto.InternalProductPurchaseRequest;
 import com.ecommerce.product.dto.InternalProductValidateRequest;
+import com.ecommerce.product.dto.UpdateProductRequest;
 import com.ecommerce.product.exception.application.ProductNotFoundException;
 import com.ecommerce.product.persistence.ProductEntity;
 import com.ecommerce.product.persistence.ProductRepository;
@@ -34,6 +36,25 @@ public class ProductService {
             }
             serverProduct.validate(requestProduct);
         });
+    }
 
+    public void update(final UpdateProductRequest request){
+        final NonDuplicatedList<Product> requestProducts = request.toProductList();
+        final List<Long> requestProductEntityIds = requestProducts.getStream().map(product->ProductEntity.from(product).getProductId()).toList();
+        final List<ProductEntity> serverProductEntities = productRepository.findAllById(requestProductEntityIds);
+        if(serverProductEntities.isEmpty()){
+            throw new ProductNotFoundException("서버에 상품이 존재하지 않습니다.");
+        }
+        final List<Product> serverProducts = serverProductEntities.stream().map(i->i.toProduct()).toList();
+        final Map<Product,Product> serverProductMap = serverProducts.stream().collect(Collectors.toMap(i->i,i->i));
+        final List<Product> resultProducts = requestProducts.getStream().map(requestProduct->{
+            final Product serverProduct = serverProductMap.get(requestProduct);
+            if(serverProduct==null){
+                throw new ProductNotFoundException("서버에 존재하지않는 상품이 있습니다.");
+            }
+            return serverProduct.update(requestProduct);
+        }).toList();
+        final List<ProductEntity> resultProductEntities = resultProducts.stream().map(i->ProductEntity.from(i)).toList();
+        productRepository.saveAll(resultProductEntities);
     }
 }
