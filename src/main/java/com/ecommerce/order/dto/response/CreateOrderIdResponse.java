4ef51ca83b06aeff11bf4b55165e979d0f8a5710
Seun Yi <yiseun10@gmail.com>
2025-04-29 20:45:33 +0900

    feat : order dto 보완

--- src/main/java/com/ecommerce/order/dto/response/CreateOrderIdResponse.java
+++ src/main/java/com/ecommerce/order/dto/response/CreateOrderIdResponse.java
@@ -1,21 +1,34 @@
 package com.ecommerce.order.dto.response;
 
 import com.ecommerce.order.domain.TmpOrder;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import com.ecommerce.order.dto.OrderItemDto;
+import lombok.*;
+
+import java.util.List;
 
 @Builder
+@EqualsAndHashCode
 @Getter
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public class CreateOrderIdResponse {
     private final String orderId;
+    private final List<OrderItemDto> orderItemDtos;
 
     public static CreateOrderIdResponse from(final TmpOrder tmpOrder){
-        return new CreateOrderIdResponse(tmpOrder.getOrderId().getValue().toString());
+        final String orderId = tmpOrder.getOrderId().getValue().toString();
+        final List<OrderItemDto> orderItemDtoList = tmpOrder.getTmpOrderItems().stream().map(i->
+                OrderItemDto.builder()
+                        .orderItemId(i.getOrderItemId().getValue().toString())
+                        .productId(i.getProductId())
+                        .productName(i.getProductName())
+                        .quantity(i.getQuantity())
+                        .price(i.getPrice())
+                        .discountPrice(i.getDiscountPrice())
+                        .couponDiscountPercent(i.getCouponDiscountPercent())
+                        .couponId(i.getCouponId())
+                        .userCouponId(i.getUserCouponId())
+                        .build()
+        ).toList();
+        return new CreateOrderIdResponse(orderId,orderItemDtoList);
     }
 }
-
-//변경사항
-//1.패키지 위치 변경
