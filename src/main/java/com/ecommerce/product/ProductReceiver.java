ed619607bcc4d1b3bdc595941e45ed992a2fb7a3
Seun Yi <yiseun10@gmail.com>
2025-04-29 19:13:12 +0900

    feat : product receiver에update 메서드 추가

--- src/main/java/com/ecommerce/product/ProductReceiver.java
+++ src/main/java/com/ecommerce/product/ProductReceiver.java
@@ -1,5 +1,6 @@
 package com.ecommerce.product;
 
+import com.ecommerce.product.dto.InternalProductPurchaseRequest;
 import com.ecommerce.product.dto.InternalProductValidateRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -10,5 +11,9 @@ public class ProductReceiver {
     private final ProductService productService;
     public void validate(final InternalProductValidateRequest request){
         productService.validate(request);
+    }
+
+    public void purchase(final InternalProductPurchaseRequest request){
+        productService.update(request);
     }
 }
