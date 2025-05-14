4ea5fa829f900c0f5ddcc8698b9b23bfb916eb8e
Seun Yi <yiseun10@gmail.com>
2025-05-15 01:38:00 +0900

    feat : prevalidation domain 추가

---
+++ src/main/java/com/ecommerce/auth/domain/prevalidation/AccessCode.java
@@ -0,0 +1,19 @@
+package com.ecommerce.auth.domain.prevalidation;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+@EqualsAndHashCode
+@Getter
+public class AccessCode {
+    private final String value;
+    private AccessCode(final String value){
+        this.value = value;
+    }
+    public boolean isEmpty(){
+        return this.value == null;
+    }
+    public static AccessCode from(final String randomCode){
+        return new AccessCode(randomCode);
+    }
+}
---
+++ src/main/java/com/ecommerce/auth/domain/prevalidation/ValidateInfo.java
@@ -0,0 +1,55 @@
+package com.ecommerce.auth.domain.prevalidation;
+
+import com.ecommerce.auth.exception.application.domain.InvalidConstructionException;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+@EqualsAndHashCode
+@Getter
+public class ValidateInfo {
+    private static final int DEFAULT_TRY_COUNT = 10;
+    private static final int MINIMUM_TRY_COUNT = 0;
+    private static final int TRY_COUNT_REDUCED_PER_TRY = 1;
+    private final AccessCode accessCode;
+    private final Integer tryCount;
+    private final ValidateState validateState;
+
+    private ValidateInfo(final AccessCode accessCode, final Integer tryCount, final ValidateState validateState){
+        this.accessCode = validate(accessCode);
+        this.tryCount = tryCount;
+        this.validateState = validateState;
+    }
+    private AccessCode validate(final AccessCode accessCode){
+        if(accessCode==null){
+            throw new InvalidConstructionException("accessCode는 필수조건입니다.");
+        }
+        return accessCode;
+    }
+    public boolean isEmpty(){
+        return this.accessCode.isEmpty();
+    }
+    public String getValidateState(){
+        return this.validateState.name();
+    }
+
+    public static ValidateInfo of(final AccessCode accessCode,final Integer tryCount,final String validateState){
+        return new ValidateInfo(accessCode, tryCount, ValidateState.from(validateState));
+    }
+
+    private enum ValidateState{
+        PREPARE,
+        FAIL,
+        SUCCESS;
+
+        public static ValidateState from(final String value){
+            if(value==null){
+                return null;
+            }
+            try{
+                return ValidateState.valueOf(value);
+            }catch (IllegalArgumentException e){
+                throw new InvalidConstructionException("존재하지 않는 값입니다.");
+            }
+        }
+    }
+}
