cceb60368de7305bc502d9ff81a62999fd229622
Seun Yi <yiseun10@gmail.com>
2025-05-16 22:13:04 +0900

    feat : auth controller endpoint 추가

--- src/main/java/com/ecommerce/auth/ui/AuthController.java
+++ src/main/java/com/ecommerce/auth/ui/AuthController.java
@@ -1,9 +1,7 @@
 package com.ecommerce.auth.ui;
 
 import com.ecommerce.auth.AuthService;
-import com.ecommerce.auth.dto.PrevalidationSessionDto;
-import com.ecommerce.auth.dto.SendVerifyMailRequest;
-import com.ecommerce.auth.dto.SendVerifyMailRequestBody;
+import com.ecommerce.auth.dto.*;
 import com.ecommerce.auth.port.AuthClientRegistry;
 import com.ecommerce.auth.ui.session.PrevalidationSession;
 import lombok.RequiredArgsConstructor;
@@ -19,10 +17,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
     private final AuthClientRegistry authClientRegistry;
     private final AuthService authService;
-    @PostMapping("/sendMail")
+    @PostMapping("/mail/send")
     public ResponseEntity<Void> sendVerifyMail(final PrevalidationSession prevalidationSession, @RequestBody final SendVerifyMailRequestBody body){
         final PrevalidationSessionDto response = authService.createPrevalidation(prevalidationSession.getPrevalidationSessionData(), SendVerifyMailRequest.of(authClientRegistry.getCreatePrevalidationClient(),body));
         prevalidationSession.update(response);
         return ResponseEntity.ok().build();
+    }
+    @PostMapping("/mail/verify")
+    public ResponseEntity<Void> verifyAccessCode(final PrevalidationSession prevalidationSession, @RequestBody final VerifyAccessCodeRequest request){
+        final PrevalidationSessionDto response = authService.updatePrevalidation(prevalidationSession.getPrevalidationSessionData(),request);
+        prevalidationSession.update(response);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/member/sign-up")
+    public ResponseEntity<SignUpResponse> createMember(final PrevalidationSession prevalidationSession, @RequestBody final SignUpRequest request){
+        final SignUpResponse response = authService.createAuth(prevalidationSession.getPrevalidationSessionData(),request);
+        return ResponseEntity.ok(response);
     }
 }
