19e6216dcb8b327ee78a1ef882ecff2af168763f
Seun Yi <yiseun10@gmail.com>
2025-05-27 00:56:03 +0900

    feat : order dto 추가

---
+++ src/main/java/com/ecommerce/order/dto/request/CancelOrderRequest.java
@@ -0,0 +1,53 @@
+package com.ecommerce.order.dto.request;
+
+import com.ecommerce.order.domain.Order;
+import com.ecommerce.order.domain.OrderBase;
+import com.ecommerce.order.domain.OrderDetail;
+import com.ecommerce.order.domain.OrderId;
+import com.ecommerce.order.domain.orderitem.*;
+import com.ecommerce.order.domain.orderitem.OrderItemState;
+import com.ecommerce.order.port.OrderCancelClient;
+import com.ecommerce.order.port.OrderUpdateClient;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public class CancelOrderRequest implements UpdateOrderRequest{
+    private final String memberId;
+    private final CancelOrderRequestBody body;
+    private final OrderCancelClient client;
+    @Override
+    public Order toOrder() {
+        final OrderBase orderBase = OrderBase.of(OrderId.from(body.getOrderId()),this.memberId);
+        final OrderDetail orderDetail = OrderDetail.createEmpty();
+        final Price price = Price.from(body.getTotalPrice());
+        final List<OrderItem> orderItems = body.getOrderItemDtos().stream().map(orderItemDto -> {
+            final OrderItemInfo orderItemInfo = OrderItemInfo.builder()
+                    .orderItemId(OrderItemId.from(orderItemDto.getOrderItemId()))
+                    .productId(orderItemDto.getProductId())
+                    .productName(orderItemDto.getProductName())
+                    .quantity(Quantity.from(orderItemDto.getQuantity()))
+                    .price(Price.from(orderItemDto.getPrice()))
+                    .discountPrice(Price.from(orderItemDto.getDiscountPrice()))
+                    .couponDiscountPercent(orderItemDto.getCouponDiscountPercent())
+                    .couponId(orderItemDto.getCouponId())
+                    .userCouponId(orderItemDto.getUserCouponId())
+                    .build();
+            final OrderItemState orderItemState = OrderItemState.CANCELED;
+            final TrackingInfo trackingInfo = TrackingInfo.createEmpty();
+            return OrderItem.of(orderItemInfo,orderItemState,trackingInfo);
+        }).toList();
+        return Order.createRequestOrder(orderBase,orderDetail,price,orderItems);
+    }
+
+    @Override
+    public OrderUpdateClient getClient() {
+        return this.client;
+    }
+
+    public static CancelOrderRequest of(final String memberId,final CancelOrderRequestBody body,final OrderCancelClient client){
+        return new CancelOrderRequest(memberId, body, client);
+    }
+}
---
+++ src/main/java/com/ecommerce/order/dto/request/CancelOrderRequestBody.java
@@ -0,0 +1,15 @@
+package com.ecommerce.order.dto.request;
+
+import com.ecommerce.order.dto.OrderItemDto;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@RequiredArgsConstructor
+public class CancelOrderRequestBody {
+    private final List<OrderItemDto> orderItemDtos;
+    private final String orderId;
+    private final String totalPrice;
+}
