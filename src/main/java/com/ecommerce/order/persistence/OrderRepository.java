1762dd34d43fe0526d1524c3543a6edb8e56b422
Seun Yi <yiseun10@gmail.com>
2025-05-27 01:00:17 +0900

    feat : orderId로 찾는 메서드 추가

--- src/main/java/com/ecommerce/order/persistence/OrderRepository.java
+++ src/main/java/com/ecommerce/order/persistence/OrderRepository.java
@@ -4,6 +4,10 @@ import com.ecommerce.order.persistence.entity.OrderEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface OrderRepository extends JpaRepository<OrderEntity,Long> {
+
+    Optional<OrderEntity> findByOrderId(final String orderId);
 }
