ef7cd32a39b185fce27ba0e5f4b904ddc5926e32
Seun Yi <yiseun10@gmail.com>
2025-05-30 19:22:36 +0900

    feat : order read 메서드 추가

--- src/main/java/com/ecommerce/order/OrderController.java
+++ src/main/java/com/ecommerce/order/OrderController.java
@@ -2,13 +2,11 @@ package com.ecommerce.order;
 
 import com.ecommerce.order.dto.request.*;
 import com.ecommerce.order.dto.response.CreateOrderIdResponse;
+import com.ecommerce.order.dto.response.ReadOrderResponse;
 import com.ecommerce.order.port.OrderClientRegistry;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/order")
@@ -24,20 +22,37 @@ public class OrderController {
     }
 
     @PostMapping("/complete")
-    public ResponseEntity<Void> completeOrder(final String memberId, @RequestBody CompleteOrderRequestBody body){
+    public ResponseEntity<Void> completeOrder(final String memberId, @RequestBody final CompleteOrderRequestBody body){
         orderService.createOrder(CompleteOrderRequest.of(memberId,body,registry.getOrderCompleteClient()));
         return ResponseEntity.ok().build();
     }
 
     @PostMapping("/cancel")
-    public ResponseEntity<Void> cancelOrder(final String memberId, @RequestBody CancelOrderRequestBody body){
+    public ResponseEntity<Void> cancelOrder(final String memberId, @RequestBody final CancelOrderRequestBody body){
         orderService.updateOrder(CancelOrderRequest.of(memberId,body,registry.getOrderCancelClient()));
         return ResponseEntity.ok().build();
     }
 
     @PostMapping("/trackingInfo/register")
-    public ResponseEntity<Void> registerTrackingInfo(final String memberId, @RequestBody RegisterTrackingInfoRequestBody body){
+    public ResponseEntity<Void> registerTrackingInfo(final String memberId, @RequestBody final RegisterTrackingInfoRequestBody body){
         orderService.updateOrder(RegisterTrackingInfoRequest.of(memberId,body,registry.getNoOperationClient()));
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<ReadOrderResponse> readOrder(final String memberId,
+                                                       @RequestParam(required = false) final String orderId,
+                                                       @RequestParam(required = false) final String startDate,
+                                                       @RequestParam(required = false) final String endDate,
+                                                       @RequestParam(required = false) final String size){
+        final ReadOrderRequest request = ReadOrderRequest.builder()
+                .memberId(memberId)
+                .orderId(orderId)
+                .startDate(startDate)
+                .endDate(endDate)
+                .size(size)
+                .build();
+        final ReadOrderResponse response = orderService.findOrder(request);
+        return ResponseEntity.ok(response);
     }
 }
--- src/main/java/com/ecommerce/order/persistence/OrderRepository.java
+++ src/main/java/com/ecommerce/order/persistence/OrderRepository.java
@@ -1,13 +1,19 @@
 package com.ecommerce.order.persistence;
 
 import com.ecommerce.order.persistence.entity.OrderEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface OrderRepository extends JpaRepository<OrderEntity,Long> {
+public interface OrderRepository extends JpaRepository<OrderEntity,Long> , JpaSpecificationExecutor<OrderEntity> {
 
     Optional<OrderEntity> findByOrderId(final String orderId);
+    @Override
+    Page<OrderEntity> findAll(Specification<OrderEntity> spec, Pageable pageable);
 }
