ad1e37fb8eaa4cc55c338f79d60a757cd7b26f7a
Seun Yi <yiseun10@gmail.com>
2025-06-15 11:41:28 +0900

    feat : order dto 수정 및 추가

--- src/main/java/com/ecommerce/order/dto/OrderDto.java
+++ src/main/java/com/ecommerce/order/dto/OrderDto.java
@@ -1,12 +1,17 @@
 package com.ecommerce.order.dto;
 
+import lombok.Builder;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 
+@Builder
+@Getter
 @RequiredArgsConstructor
 public class OrderDto {
     private final String orderId;
+    private final String memberId;
     private final String totalPrice;
     private final List<OrderItemDto> orderItemDtos;
 }
