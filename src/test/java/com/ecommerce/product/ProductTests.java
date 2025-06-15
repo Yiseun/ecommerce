775a7f9f9bed3ca83d56e00997d00034cd9bb2f2
Seun Yi <yiseun10@gmail.com>
2025-06-15 12:20:10 +0900

    test : product test 수정

--- src/test/java/com/ecommerce/product/ProductTests.java
+++ src/test/java/com/ecommerce/product/ProductTests.java
@@ -3,6 +3,7 @@ package com.ecommerce.product;
 import static org.assertj.core.api.Assertions.*;
 import com.ecommerce.product.domain.Product;
 import com.ecommerce.product.domain.ProductInfo;
+import com.ecommerce.product.domain.Quantity;
 import com.ecommerce.product.exception.application.domain.BusinessLogicException;
 import org.junit.jupiter.api.Test;
 
@@ -11,8 +12,8 @@ public class ProductTests {
     @Test
     void requestProduct가_serverProduct에_유효하지않은_상품이라면_실패한다(){
         final ProductInfo commonProductInfo = ProductInfo.of("3241","hdsfdsf","234324");
-        final Product requestProduct = Product.of(commonProductInfo,"100");
-        final Product serverProduct = Product.of(commonProductInfo,"5");
+        final Product requestProduct = Product.of(commonProductInfo, Quantity.from("100"));
+        final Product serverProduct = Product.of(commonProductInfo,Quantity.from("5"));
 
         assertThatThrownBy(()->serverProduct.validate(requestProduct)).isInstanceOf(BusinessLogicException.class);
     }
