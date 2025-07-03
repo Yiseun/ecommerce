633f40001192d3c06e05556dae0b2ac8a5905af1
Seun Yi <yiseun10@gmail.com>
2025-07-03 18:23:48 +0900

    fix : lock 획득 쿼리 수정

--- src/main/java/com/ecommerce/product/persistence/ProductRepository.java
+++ src/main/java/com/ecommerce/product/persistence/ProductRepository.java
@@ -1,8 +1,18 @@
 package com.ecommerce.product.persistence;
 
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface ProductRepository extends JpaRepository<ProductEntity,Long> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select p from ProductEntity p where p.id = :id")
+    Optional<ProductEntity> findByIdWithPessimisticWriteLock(@Param("id") Long id);
 }
