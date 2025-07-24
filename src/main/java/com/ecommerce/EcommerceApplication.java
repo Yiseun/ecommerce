a483169afbac29a5acd49531c754256dc231f535
Seun Yi <yiseun10@gmail.com>
2025-07-25 00:05:18 +0900

    feat : 어노테이션추가

--- src/main/java/com/ecommerce/EcommerceApplication.java
+++ src/main/java/com/ecommerce/EcommerceApplication.java
@@ -3,10 +3,12 @@ package com.ecommerce;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.retry.annotation.EnableRetry;
 
 @ConfigurationPropertiesScan
+@EnableCaching
 @EnableJpaAuditing
 @EnableRetry
 @SpringBootApplication
