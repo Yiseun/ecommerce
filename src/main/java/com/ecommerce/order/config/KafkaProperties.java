9316773e780f9af08ff2654302bcbcbd69f740e0
Seun Yi <yiseun10@gmail.com>
2025-09-14 14:22:00 +0900

    feat : Order kafka 설정 추가

---
+++ src/main/java/com/ecommerce/order/config/KafkaProperties.java
@@ -0,0 +1,14 @@
+package com.ecommerce.order.config;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "kafka")
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public class KafkaProperties {
+    private final String bootstrapServers;
+    private final String groupId;
+}
